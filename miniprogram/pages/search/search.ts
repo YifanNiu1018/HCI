8750a3710421885d1997d2ebe3701d18e1eceb20
Li Shu <1569135738@qq.com>
2025-12-13 22:57:40 +0800

    顶部为手机状态栏留空
    顶部为手机状态栏留空（custom导航下不会自行预留）

--- miniprogram/pages/search/search.ts
+++ miniprogram/pages/search/search.ts
@@ -1,4 +1,13 @@
 Page({
-
-})
-
+    data: {
+      mode: 'recipe' // recipe | ingredient
+    },
+  
+    switchMode(e) {
+      const { mode } = e.currentTarget.dataset
+      if (mode === this.data.mode) return
+  
+      this.setData({ mode })
+    }
+  })
+  
